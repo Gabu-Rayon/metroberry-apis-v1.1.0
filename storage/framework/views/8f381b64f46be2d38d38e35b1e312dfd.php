3c9fc1c0dfb5a5288f247eef930794b361c72b41
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-10-14 14:19:36 +0300

    Commit change made on  users avatars

--- storage/framework/views/8f381b64f46be2d38d38e35b1e312dfd.php
+++ storage/framework/views/8f381b64f46be2d38d38e35b1e312dfd.php
@@ -34,6 +34,19 @@
 
             <div class="rest-container">
                 <div class="address-title">Personal ID</div>
+                <?php if(session('success')): ?>
+                    <div id="success-message" class="alert alert-success" style="display: none;">
+                        <?php echo e(session('success')); ?>
+
+                    </div>
+                <?php endif; ?>
+
+                <?php if(session('error')): ?>
+                    <div id="error-message" class="alert alert-danger" style="display: none;">
+                        <?php echo e(session('error')); ?>
+
+                    </div>
+                <?php endif; ?>
 
                 <!--Driver's License Fields Container Start-->
                 <div class="all-container all-container-with-classes">
@@ -54,70 +67,68 @@
                             </div>
                         </div>
                         <!--Input Field Container End-->
-
-                        <!--Upload Front Start-->
-                        <div class="display-flex justify-content-between">
-                            <span class="position-relative upload-btn">
-                                <img src=" <?php echo e(asset('mobile-app-assets/icons/upload.svg')); ?>" alt="Upload Icon" />
-                                <input class="scan-prompt" type="file" accept="image/*"
-                                    name="national_id_front_avatar" />
-                            </span>
-                            <span class="text-uppercase">FRONT</span>
-                            <span class="delete-btn">
-                                <img src=" <?php echo e(asset('mobile-app-assets/icons/delete.svg')); ?>" alt="Delete Icon" />
-                            </span>
-                        </div>
-                        <div class="scan-your-card-prompt margin-top-5">
-                            <div class="position-relative">
-                                <div class="upload-picture-container">
-                                    <div class="upload-camera-container text-center">
-                                        <span class="#">
-                                            
-                                            <img id="national-id-front-preview"
-                                                src="<?php echo e($driver->national_id_front_avatar
-                                                    ? asset('storage/' . $driver->national_id_front_avatar)
-                                                    : asset('mobile-app-assets/icons/photocamera.svg')); ?>"
-                                                alt="National ID Behind" class="img-fluid" />
-                                        </span>
+                        <!-- Upload Front national ID -->
+                        <div class="form-group">
+                            <label class="width-100">
+                                <div class="display-flex justify-content-between">
+                                    <span class="position-relative upload-btn">
+                                        <img src="<?php echo e(asset('mobile-app-assets/icons/upload.svg')); ?>" alt="Upload Icon" />
+                                        <input class="scan-prompt" type="file" accept="image/*"
+                                            name="national_id_front_avatar" id="national-id-back-input" />
+                                    </span>
+                                    <span class="text-uppercase">National ID BACK</span>
+                                    <span class="delete-btn" id="national-id-back-delete">
+                                        <img src="<?php echo e(asset('mobile-app-assets/icons/delete.svg')); ?>" alt="Delete Icon" />
+                                    </span>
+                                </div>
+                                <div class="scan-your-card-prompt margin-top-5">
+                                    <div class="position-relative">
+                                        <div class="upload-picture-container">
+                                            <div class="upload-camera-container text-center">
+                                                <span class="#">
+                                                    <img id="national-id-back-preview"
+                                                        src="<?php echo e($driver->national_id_behind_avatar
+                                                            ? asset('storage/' . $driver->national_id_behind_avatar)
+                                                            : asset('mobile-app-assets/icons/photocamera.svg')); ?>"
+                                                        alt="National ID Back" />
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <input class="scan-prompt" type="file" accept="image/*" />
-                            </div>
+                            </label>
                         </div>
-                        <!--Upload Front End-->
-
-                        <!--Upload Back Start-->
-                        <div class="display-flex justify-content-between">
-                            <span class="position-relative upload-btn">
-                                <img src=" <?php echo e(asset('mobile-app-assets/icons/upload.svg')); ?>" alt="Upload Icon" />
-                                <input class="scan-prompt" type="file" accept="image/*"
-                                    name="national_id_behind_avatar" />
-                            </span>
-                            <span class="text-uppercase">BACK</span>
-                            <span class="delete-btn">
-                                <img src=" <?php echo e(asset('mobile-app-assets/icons/delete.svg')); ?>" alt="Delete Icon" />
-                            </span>
-                        </div>
-                        <div class="scan-your-card-prompt margin-top-5">
-                            <div class="position-relative">
-                                <div class="upload-picture-container">
-                                    <div class="upload-camera-container text-center">
-                                        <span class="#">
-                                            
-                                            <img id="national-id-front-preview"
-                                                src="<?php echo e($driver->national_id_behind_avatar
-                                                    ? asset('storage/' . $driver->national_id_behind_avatar)
-                                                    : asset('mobile-app-assets/icons/photocamera.svg')); ?>"
-                                                alt="National ID Behind" class="img-fluid" />
-                                        </span>
+                        <!-- Upload Front national ID -->
+                        <div class="form-group">
+                            <label class="width-100">
+                                <div class="display-flex justify-content-between">
+                                    <span class="position-relative upload-btn">
+                                        <img src="<?php echo e(asset('mobile-app-assets/icons/upload.svg')); ?>" alt="Upload Icon" />
+                                        <input class="scan-prompt" type="file" accept="image/*"
+                                            name="national_id_back_avatar" id="national-id-front-input" />
+                                    </span>
+                                    <span class="text-uppercase">National ID Behind</span>
+                                    <span class="delete-btn" id="national-id-front-delete">
+                                        <img src="<?php echo e(asset('mobile-app-assets/icons/delete.svg')); ?>" alt="Delete Icon" />
+                                    </span>
+                                </div>
+                                <div class="scan-your-card-prompt margin-top-5">
+                                    <div class="position-relative">
+                                        <div class="upload-picture-container">
+                                            <div class="upload-camera-container text-center">
+                                                <span class="#">
+                                                    <img id="national-id-front-preview"
+                                                        src="<?php echo e($driver->national_id_behind_avatar
+                                                            ? asset('storage/' . $driver->national_id_behind_avatar)
+                                                            : asset('mobile-app-assets/icons/photocamera.svg')); ?>"
+                                                        alt="National ID Front" />
+                                                </span>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <input class="scan-prompt" type="file" accept="image/*" />
-                            </div>
+                            </label>
                         </div>
-                        <!--Upload Front End-->
-
-
                         <div class="form-submit-button text-center">
                             <button type="submit" class="btn btn-dark text-uppercase">
                                 Update
