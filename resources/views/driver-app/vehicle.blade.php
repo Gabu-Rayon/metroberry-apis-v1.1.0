0485880ff50f84e89e7eecbf8b4bfb9b72416baa
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-12-13 00:31:34 +0300

    `Updated vehicle.blade.php: removed text class and added text class to another i tag`

--- resources/views/driver-app/vehicle.blade.php
+++ resources/views/driver-app/vehicle.blade.php
@@ -34,9 +34,8 @@
             </div>
             <br><br>
             <!--Page Title & Icons End-->
-            <i class="font-italic text-muted text-center text-info">When assigned a registered vehicle, <br>details will appear
+            <i class="font-italic text-info text-muted text-center ">When assigned a registered vehicle, <br>details will appear
                 here!<br> Registration Closed For Now !</i>
-
 
             <div class="rest-container">
                 <div class="address-title"></div>
